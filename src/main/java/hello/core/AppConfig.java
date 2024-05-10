2edba8bd86b312248aab7a8d2e6cff8b56d65fc7
youngkwonJ <j4yxvrfgqh@privaterelay.appleid.com>
2024-05-11 05:44:50 +0900

    Spring change dev note +

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,6 +10,8 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /*
 * AppConfig -> 나의 어플리케이션을 전체를 설정하고 구성한다는 뜻
@@ -17,22 +19,24 @@ import hello.core.order.OrderServiceImpl;
 * (Dependency Injection)
 *
 * */
+@Configuration // 설정 정보 , 어플리케이션의 어떤 설정정보 등을 뜻하는 어노테이션
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return  new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepostitory memberRepository() {
+    @Bean
+    public MemberRepostitory memberRepository() {
         return new MemoryMemberRepository();
     }
 
-
+    @Bean
     public OrderService orderService(){
 
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
        /* return new FixiDiscountPolicy();*/
         return new RateDiscountPolicy();
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,13 +7,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-
+/*
         AppConfig appConfig= new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService();*/
+
+        // 어플리케이션 객체 를 통해 메인 컨테이너를 불러오겠다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // applicationContext 객체의 .getBean메서드를 활용하여 appConfig 메인 스프링 컨테이너 의
+        // 등록된 bean 중에 이름을 memberService 라 붙혀있는 bean을 끌어오겠다
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // applicationContext 객체의 .getBean메서드를 활용하여 appConfig 메인 스프링 컨테이너 의
+        // 등록된 bean 중에 이름을 orderService  라 붙혀있는 bean을 끌어오겠다
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
 
         Long memberId = 1L;
