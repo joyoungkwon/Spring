46b23a1dd1ddaca33104ed74bb57af94d16bb302
youngkwonJ <j4yxvrfgqh@privaterelay.appleid.com>
2024-05-16 06:20:48 +0900

    Thredd ex 다발성 이슈 처리 test code +

--- src/test/java/hello/core/singleton/StateFulService.java
+++ src/test/java/hello/core/singleton/StateFulService.java
@@ -1,14 +1,10 @@
 package hello.core.singleton;
 
 public class StateFulService {
-    private int price ;//
 
-    public void order(String name , int price){
-        System.out.println("name = " + name + "price" + price);
-        this.price = price;
-    }
 
-    public  int getPrice(){
-       return price;
+    public int order(String name , int price){
+
+      return price;
     }
 }
