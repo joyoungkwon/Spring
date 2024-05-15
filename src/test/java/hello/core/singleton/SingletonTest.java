ac57f899f69b79083113a7033e92e2fd3a5b33ce
youngkwonJ <j4yxvrfgqh@privaterelay.appleid.com>
2024-05-16 05:10:25 +0900

    SingletonService, SingletonTest + DEV NOTE UPDATE+

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -29,5 +31,23 @@ public class SingletonTest {
        SingletonService singletonService1 = SingletonService.getInstance();
        SingletonService singletonService2 = SingletonService.getInstance();
        Assertions.assertThat(singletonService2).isSameAs(singletonService1);
+    }
+
+    @Test
+    @DisplayName(" spring  container")
+    void SpringContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService_USER1 = ac.getBean("memberService",MemberService.class);
+        MemberService memberService_USER2 = ac.getBean("memberService",MemberService.class);
+
+        System.out.println("memberService_USER1 = " + memberService_USER1);
+        System.out.println("memberService_USER2 = " + memberService_USER2);
+
+        /*
+        (Instance) USER1 == USER2 -> T
+        (Instance) USER1 != USER2 -> F
+
+        * */
+        Assertions.assertThat(memberService_USER1).isSameAs(memberService_USER2);
     }
 }
