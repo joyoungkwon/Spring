2b61599542d8ab93a6a7d512276fee42ba8b1a07
youngkwonJ <j4yxvrfgqh@privaterelay.appleid.com>
2024-05-23 03:00:53 +0900

    request 예제 + dev not

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,48 @@
+package hello.core.common;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/*
+* 순서: 컨테이너에 http request 요청시 하나씩 request scope 하나씩 생성 처리 후 소멸
+* 1 @PostConstruct 으로 첫번쨰 생성 uuid = UUID.RANDOM으로 UUID 하나씩 생성
+* 2 public void log() 메서드 실행
+* 3 @PreDestroy 로 마지막 close () 메서드 실행
+* requestUrl은 생성 시점을 알수 없어서 외부에서 주입.(조립)
+* */
+
+@Component // 스캔
+@Scope(value = "request") // http 가 요청 될떄 요청당 하나씩 생기고 요청이 종료되면 소멸 되는 범위.(lifeCycle)
+public class MyLogger {
+    private String uuid;
+    private String RequestURL;
+
+
+    public void setRequestURL(String requestURL){
+
+        requestURL = this.RequestURL;
+    }
+
+    public void log (String message){
+
+        System.out.println("["+uuid+"]" + "["+RequestURL+"]" + " : "+ message );
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString(); // 생성 새로운거 생성하는거임 그냥
+        System.out.println("[" + uuid +"]" + "request scope bean create " + this );
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid +"]" + "request scope bean close " + this );
+
+    }
+}
